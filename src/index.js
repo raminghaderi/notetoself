4a2794415df708271f86876a0f40334763026312
Ramin Ghaderi <ramin.ghaderi@pentacor.de>
2020-05-19 00:26:25 +0200

    note to self completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
+import "./index.css";
 
-ReactDOM.render(<div>App</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
